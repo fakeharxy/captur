983c6574617a0336d4ff1b630fa7a43c37b4bd53
harxy <harkerp@gmail.com>
2017-09-10 17:12:53 +0100

    got two way data binding working

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,13 @@ class App extends Component {
     super(props);
     this.state = {
       notes: [],
+      noteNumber: 0,
     };
+
+    this.onNext = this.onNext.bind(this);
+    this.onPrev = this.onPrev.bind(this);
   }
+
 
   componentDidMount() {
     window.fetch('api/notes')
@@ -18,23 +23,51 @@ class App extends Component {
       .then(notes => this.setupNotes(notes))
   }
 
+  onNext() {
+    var noteNumber = this.state.noteNumber
+    noteNumber++
+    this.setState ({ noteNumber })
+  }
+
+  onPrev() {
+    var noteNumber = this.state.noteNumber
+    noteNumber--
+    this.setState ({ noteNumber })
+  }
+
   setupNotes(notes) {
     this.setState({ notes });
   }
 
   render() {
-    const { notes } = this.state
+    const { notes, noteNumber } = this.state
     return (
       <div className="App">
-      { notes.map(item =>
-        <div key={item.id}>
+      { notes[noteNumber] ? (
+        <div key={notes[noteNumber].id}>
           <span>
-            <p>{item.body}</p>
+            <p>{notes[noteNumber].body}</p>
           </span>
         </div>
-      ) }
-
-      </div>
+      ) : (
+          <div> No more notes </div>
+        )
+      }
+      <span>
+              <button
+                onClick={() => this.onPrev()}
+                type="button"
+> Prev
+              </button>
+            </span>
+      <span>
+              <button
+                onClick={() => this.onNext()}
+                type="button"
+> Next
+              </button>
+            </span>
+     </div> 
     );
   }
 }
