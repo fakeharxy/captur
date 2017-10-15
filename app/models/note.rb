ef66bc896ea984c60aa71fd8d916e767a931b211
harxy <harkerp@gmail.com>
2017-10-15 16:31:49 +0100

    added tag models

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,18 @@
 class Note < ApplicationRecord
-  
   has_many :taggings
   has_many :tags, through: :taggings
 
   def self.order_by_last_seen
-    self.all.order(last_seen: :desc)
+    all.order(last_seen: :desc)
+  end
+
+  def self.all_tags=(names)
+    names.split(',').map do |name|
+      Tag.create!(name: name)
+    end
+  end
+
+  def self.all_tags
+    Tag.all.map(&:name).join(", ")
   end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Note do
-  it 'gets and orders notes by oldest first' do
-    Note.create!(body: 'First', last_seen: DateTime.now + 5)
-    Note.create!(body: 'Last', last_seen: DateTime.now + 10)
-    expect(Note.order_by_last_seen[0].body).to eq('Last')
+  context 'creating notes' do
+    it 'gets and orders notes by oldest first' do
+      Note.create!(body: 'First', last_seen: DateTime.now + 5)
+      Note.create!(body: 'Last', last_seen: DateTime.now + 10)
+      expect(Note.order_by_last_seen[0].body).to eq('Last')
+    end
+  end
+
+  context 'creating tags' do
+    it 'adds multiples tags to the database' do
+      Note.all_tags = 'Mike,Bob,Terry'
+      expect(Note.all_tags).to eq("Mike, Bob, Terry")
+      Note.all
+    end
   end
 end
